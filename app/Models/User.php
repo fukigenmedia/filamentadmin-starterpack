c78fa2cc8ab1a7841329d4bf23607ec7e3ea5a4d
abela-a <abelardhana96@gmail.com>
2022-02-03 12:28:43 +0800

    [install] filament-shield

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
-    use HasApiTokens, HasFactory, Notifiable, UUID;
+    use HasApiTokens, HasFactory, Notifiable, UUID, HasRoles;
 
     protected $fillable = [
         'name',
